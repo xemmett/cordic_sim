e058dbc0ddb0a9b613c92ccc7b276386487b0066
cgon2108 <54952292+cgon2108@users.noreply.github.com>
2022-04-01 15:50:16 +0100

    Add files via upload

--- verilog/Cordic1.v
+++ verilog/Cordic1.v
@@ -1,8 +1,8 @@
 /*
  * Cordic1.v - Implementation of a 2.16 Cordic Processor
  * Co-Ordinate Rotational by DIgital Computer
- * 
- * 
+ * Student Names: Charlie Gorey O'Neill, Emmett Lawlor, Niall Coughlan, Cian Surlis, Ciaran Hogan
+ * Id: 18222803, 18238831, , 18233074, 18238831
  */
 
 module CORDIC(output signed[1:-16] cosine,
@@ -10,38 +10,38 @@ module CORDIC(output signed[1:-16] cosine,
 	      output signed [1:-16] angle,
 	      output 		    done,
 	      input signed [1:-16]  target_angle,
-	      input 		    init, clk);
+	      input 		    init, 
+	      input 		    clk);
 
    reg signed [1:-16] 		    current_ang, delta_ang, new_ang, cos, sin, new_cos, new_sin;
    reg [3:0] 			    cycle;
    reg 				    state;
    
-   // Create angle step look up table, can have 2^18 (262,144) steps, to be calculated externally and
+   // Create angle step look up table, can have up to 2^18 (262,144) steps, to be calculated externally and
    // then introduced to the program as constants
-   // Delta Angle values are scaled by 2^20 so to ensure integer arithmetic up to 16 decimal places.
+   // Delta Angle values are scaled by 2^16 so to ensure integer arithmetic up to 16 decimal places.
 
    always @(*)
      begin
 	case(cycle)
 	  
-	  0: delta_ang = 18'H0C910; //823549.664583 
-	  1: delta_ang = 18'H076B2; //486169.755256 
-	  2: delta_ang = 18'H03EB7; //256878.746667 
-	  3: delta_ang = 18'H00FFB; //130395.662762 
-	  4: delta_ang = 18'H007FF; //65450.866110 
-	  5: delta_ang = 18'H00400; //32757.339579 
-	  6: delta_ang = 18'H00200; //16382.666862 
-	  7: delta_ang = 18'H00100; //8191.833339 
-	  8: delta_ang = 18'H00080; //4095.979167 
-	  9: delta_ang = 18'H00040; //2047.997396 
-	  10: delta_ang = 18'H00020; //1023.999674 
-	  11: delta_ang = 18'H00010; //511.999959 
-	  12: delta_ang = 18'H00008; //255.999995 
-	  13: delta_ang = 18'h00004; //64.000000 
-	  14: delta_ang = 18'h00002; //32.000000 
-	  15: delta_ang = 18'h00001; //16.000000 
-	  
-
+	  0: delta_ang = 18'H0C910;
+	  1: delta_ang = 18'H076B2; 
+	  2: delta_ang = 18'H03EB7;
+	  3: delta_ang = 18'H00FFB; 
+	  4: delta_ang = 18'H01FD6; 
+	  5: delta_ang = 18'H007FF; 
+	  6: delta_ang = 18'H00400; 
+	  7: delta_ang = 18'H00200; 
+	  8: delta_ang = 18'H00100;
+	  9: delta_ang = 18'H00080; 
+	  10: delta_ang = 18'H00040;
+	  11: delta_ang = 18'H00020;
+	  12: delta_ang = 18'H00010; 
+	  13: delta_ang = 18'H00008;
+	  14: delta_ang = 18'h00004;
+	  15: delta_ang = 18'h00002;
+	  16: delta_ang = 18'h00001; 
 
 	endcase
      end
@@ -52,12 +52,12 @@ module CORDIC(output signed[1:-16] cosine,
 	   state <= 0;
 	   cycle <= 0;
 	   current_ang <= 0;
-	   cos <= 18'h9b75; 
+	   cos <= 18'h09b75; 	// This is constant K which initializes cosine.
 	   sin <= 0;
 	end
 	else begin
 	   if ( state == 0 ) begin
-	      if ( cycle == 15) begin // not sure why he uses 15 
+	      if ( cycle == 15) begin 
 		 state <= 1;
 	      end
 	      cos <= new_cos;
@@ -67,16 +67,20 @@ module CORDIC(output signed[1:-16] cosine,
 	   end
 	end
      end
-
+// Here is the cordic processing algorithm at work
    always @(*)
 	begin
 	    if ( target_angle >= angle ) begin
 		new_ang <= current_ang + delta_ang;	
-	   	new_cos <= cos- (sin >>> cycle);
-	        new_sin <= sin - (cos >>> cycle);
+	   	new_cos <= cos - (sin >>> cycle);
+	        new_sin <= sin + (cos >>> cycle);
 		    end
+	else begin 
+		new_ang <= current_ang - delta_ang;
+		new_cos <= cos + (sin >>> cycle);
+		new_sin <= sin - (cos >>> cycle);
 	 end
-
+end
    assign done = state;
    assign cosine = cos;
    assign sine = sin;
--- verilog/cordic_tb.v
+++ verilog/cordic_tb.v
@@ -1,7 +1,8 @@
 // Test bench for Cordic
-// This is code taken from the meeting w Colin
 // Tests for +- angles 
 // Edge cases are +-90 and 0 degrees
+// Student Names: Charlie Gorey O'Neill, Emmett Lawlor, Niall Coughlan, Cian Surlis, Ciaran Hogan
+// Id: 18222803, 18238831, , 18233074, 18238831
 `timescale 1ns/1ps
 
 module cordic_tb;
@@ -13,7 +14,7 @@ module cordic_tb;
 
    task run_cordic(input signed[1:-16] angle);
       begin
-	 $display("test fro input angle %d (%15.12f rads, %10.6f degs)", angle, angle/65536.0, angle*0.0008742642137616321);
+	 $display("test for input angle %d (%15.12f rads, %10.6f degs)", angle, angle/65536.0, angle*0.0008742642137616321); // Displays input angle in in 2.16, radians and degrees
 	 clk = 0;
 	 init = 0;
 	 angle_in = angle;
@@ -30,7 +31,7 @@ module cordic_tb;
 	 end
 	 #5 $display("%6t: %3d: init %1b, done %1b, angle %18b, cos %18b, sin %18b",
 		     $time, count, init, done, angle_out, cos_out, sin_out);
-	 $display("	angle %d (%15.12f), cos %d (%15.12f) sin %d (%15.12f)", 
+	 $display("angle %d (%15.12f), cos %d (%15.12f) sin %d (%15.12f)", 
 		  angle_out, angle_out/65536.0, cos_out, cos_out/65536.0, sin_out, sin_out/65536.0);
       end
    endtask
@@ -38,7 +39,7 @@ module cordic_tb;
    task run_display(input signed[1:-16] angle, input integer expected_angle, input integer expected_cosine, input integer expected_sine);
       begin
 	 run_cordic(angle);
-	 $display("Results hsould be: angle: %d, cosine: %d, sine: %d\n\n", expected_angle, expected_cosine, expected_sine);
+	 $display("Results should be: angle: %d, cosine: %d, sine: %d\n\n", expected_angle, expected_cosine, expected_sine);
       end
    endtask
 
@@ -59,9 +60,9 @@ module cordic_tb;
 
 	#100 run_display(77208, 77207, 25081, 60546); //"		" = 3*pi/8 
 
-	#100 run_display(-102944, -102945, -2, 65536); //"		" = -pi/2 
+	#100 run_display(-102944, -102945, -2, -65536); //"		" = -pi/2 
 
-	#100 run_display(-51472, -51473, 46341, -46342); //"		" = -pi/4 
+	#100 run_display(-51472, -51471, 46343, -46342); //"		" = -pi/4 
 
      end
 
