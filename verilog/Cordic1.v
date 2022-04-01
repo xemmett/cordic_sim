35d55f27f44a3b373a7a946ccbc8cfe401a0f76f
cgon2108 <54952292+cgon2108@users.noreply.github.com>
2022-04-01 14:18:30 +0100

    Add files via upload

--- verilog/Cordic1.v
+++ verilog/Cordic1.v
@@ -1,72 +1,88 @@
 /*
-* Cordic1.v - Implementation of a 2.16 Cordic Processor
-* Co-Ordinate Rotational by DIgital Computer
-* 
-* 
-*/
+ * Cordic1.v - Implementation of a 2.16 Cordic Processor
+ * Co-Ordinate Rotational by DIgital Computer
+ * 
+ * 
+ */
 
 module CORDIC(output signed[1:-16] cosine,
-		output signed[1:-16] sine,
-		output signed[1:-16] angle,
-		output done,
-		input signed[1:-16] target_angle,
-		input init, clk);
+	      output signed [1:-16] sine,
+	      output signed [1:-16] angle,
+	      output 		    done,
+	      input signed [1:-16]  target_angle,
+	      input 		    init, clk);
 
-	reg signed[1:-16] current_ang, delta_ang, new_ang, cos, sin, new_cos, new_sin;
-	reg[3:0] cycle;
-	reg state;
-	
-// Create angle step look up table, can have 2^18 (262,144) steps, to be calculated externally and
-// then introduced to the program as constants
+   reg signed [1:-16] 		    current_ang, delta_ang, new_ang, cos, sin, new_cos, new_sin;
+   reg [3:0] 			    cycle;
+   reg 				    state;
+   
+   // Create angle step look up table, can have 2^18 (262,144) steps, to be calculated externally and
+   // then introduced to the program as constants
+   // Delta Angle values are scaled by 2^20 so to ensure integer arithmetic up to 16 decimal places.
 
-	always @(*)
-	begin
-		case(cycle)
-		//	0: delta_ang = ... 
-		//	1: delta_ang = ... Put in hex
-		// So on...
-		endcase
+   always @(*)
+     begin
+	case(cycle)
+	  
+	  0: delta_ang = 18'H0C910; //823549.664583 
+	  1: delta_ang = 18'H076B2; //486169.755256 
+	  2: delta_ang = 18'H03EB7; //256878.746667 
+	  3: delta_ang = 18'H00FFB; //130395.662762 
+	  4: delta_ang = 18'H007FF; //65450.866110 
+	  5: delta_ang = 18'H00400; //32757.339579 
+	  6: delta_ang = 18'H00200; //16382.666862 
+	  7: delta_ang = 18'H00100; //8191.833339 
+	  8: delta_ang = 18'H00080; //4095.979167 
+	  9: delta_ang = 18'H00040; //2047.997396 
+	  10: delta_ang = 18'H00020; //1023.999674 
+	  11: delta_ang = 18'H00010; //511.999959 
+	  12: delta_ang = 18'H00008; //255.999995 
+	  13: delta_ang = 18'h00004; //64.000000 
+	  14: delta_ang = 18'h00002; //32.000000 
+	  15: delta_ang = 18'h00001; //16.000000 
+	  
+
+
+	endcase
+     end
+
+   always @(posedge clk)
+     begin
+	if( init == 1'b1 ) begin
+	   state <= 0;
+	   cycle <= 0;
+	   current_ang <= 0;
+	   cos <= 18'h9b75; 
+	   sin <= 0;
 	end
-
-	always @(posedge clk)
-	begin
-		if( init == 1'b1 ) begin
-		state <= 0;
-		cycle <= 0;
-		current_ang <= 0;
-	// 	cos <= K - this will have to be calculated externally and will be dependent on how
-	// many steps we decide to implement
-		sin <= 0;
-		end
-		else begin
-		if ( state = 0 ) begin
-			if ( cycle == 15) begin // not sure why he uses 15 
-				state <= 1;
-				end
-			cos <= new_cos;
-			sin <= new_sin;
-			current_ang <= new_ang;
-			cycle <= cycle + 1;
-			end
-		end
+	else begin
+	   if ( state == 0 ) begin
+	      if ( cycle == 15) begin // not sure why he uses 15 
+		 state <= 1;
+	      end
+	      cos <= new_cos;
+	      sin <= new_sin;
+	      current_ang <= new_ang;
+	      cycle <= cycle + 1;
+	   end
 	end
+     end
 
-	always @(*)
+   always @(*)
 	begin
-		if ( target_angle >= angle ) begin
-		new_ang <= current_ang + delta_ang;
-		new_cos <= cos- (sin >>> cycle);
-		new_sin <= sin - (cos >>> cycle);
-	end
-end
+	    if ( target_angle >= angle ) begin
+		new_ang <= current_ang + delta_ang;	
+	   	new_cos <= cos- (sin >>> cycle);
+	        new_sin <= sin - (cos >>> cycle);
+		    end
+	 end
 
-assign done = state;
-assign cosine = cos;
-assign sine = sin;
-assign angle = current_ang;
+   assign done = state;
+   assign cosine = cos;
+   assign sine = sin;
+   assign angle = current_ang;
 
-
-
+endmodule
 
 
 
---
+++ verilog/cordic_tb.v
@@ -0,0 +1,73 @@
+// Test bench for Cordic
+// This is code taken from the meeting w Colin
+// Tests for +- angles 
+// Edge cases are +-90 and 0 degrees
+`timescale 1ns/1ps
+
+module cordic_tb;
+   reg signed[1:-16] angle_in;
+   wire signed [1:-16] cos_out, sin_out, angle_out;
+   wire 	       done;
+   reg 		       clk, init;
+   integer 	       count;
+
+   task run_cordic(input signed[1:-16] angle);
+      begin
+	 $display("test fro input angle %d (%15.12f rads, %10.6f degs)", angle, angle/65536.0, angle*0.0008742642137616321);
+	 clk = 0;
+	 init = 0;
+	 angle_in = angle;
+	 #5 init = 1;
+	 count = 0;
+	 #5 clk = 1;
+	 #5 init = 0;
+	 #5 clk = 0;		
+	 while ( done == 1'b0 ) begin
+	    #5 clk = 1;
+	    #10 clk = 0;
+	    #5 count = count+1;
+	    init = 0;
+	 end
+	 #5 $display("%6t: %3d: init %1b, done %1b, angle %18b, cos %18b, sin %18b",
+		     $time, count, init, done, angle_out, cos_out, sin_out);
+	 $display("	angle %d (%15.12f), cos %d (%15.12f) sin %d (%15.12f)", 
+		  angle_out, angle_out/65536.0, cos_out, cos_out/65536.0, sin_out, sin_out/65536.0);
+      end
+   endtask
+
+   task run_display(input signed[1:-16] angle, input integer expected_angle, input integer expected_cosine, input integer expected_sine);
+      begin
+	 run_cordic(angle);
+	 $display("Results hsould be: angle: %d, cosine: %d, sine: %d\n\n", expected_angle, expected_cosine, expected_sine);
+      end
+   endtask
+
+   // dut - device under test
+   CORDIC dut(cos_out, sin_out, angle_out, done, angle_in, init, clk);
+
+   initial
+     begin
+	// Tests 4 arguments: 1-Input angle, 2-Expected out angle, 3-Expected Cos, 4-Expected Sine. 
+	// All arguments presented in 2.16 rads
+
+	#0 run_display(0, -1, 65536, 2);
+	#100 run_display(102944, 102945, 2, 65536); // 102944 = pi/2 radians (90 deg) in 2.16 fp format
+
+	#100 run_display(51472, 51473, 46341, 46342); //"		" = pi/4 
+
+	#100 run_display(25736, 25737, 60546, 25081); //"		" = pi/8 
+
+	#100 run_display(77208, 77207, 25081, 60546); //"		" = 3*pi/8 
+
+	#100 run_display(-102944, -102945, -2, 65536); //"		" = -pi/2 
+
+	#100 run_display(-51472, -51473, 46341, -46342); //"		" = -pi/4 
+
+     end
+
+   initial
+     begin
+	$dumpfile("Cordic1.vcd");
+	$dumpvars;
+     end
+endmodule
